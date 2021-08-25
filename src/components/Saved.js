e129403defadcbefde7496b6485fb4388807aac1
ordegani <oradeganii@gmail.com>
2021-08-26 02:26:23 +0300

    changes

--- src/components/Saved.js
+++ src/components/Saved.js
@@ -1,28 +1,34 @@
-import React from 'react'
-import Recipe from './Recipe'
+import React from "react";
+import Recipe from "./Recipe";
 import "../App.css";
 
-export default function Saved({favourites, setFavourites}) {
-    const deleteFromfavourites = (recipe) => {
-        setFavourites(
-          favourites.filter((favourite) => favourite.title !== recipe.title)
-        );
-      };
-    return (
-        <div className="favourites">
-          {favourites.map((favourite, index) => (
-            <Recipe
-              key={index}
-              title={favourite.title}
-              calories={favourite.calories}
-              image={favourite.image}
-              ingredients={favourite.ingredients}
-              onClick={deleteFromfavourites} 
-              buttonText="DELETE"
-            />
-          ))}
-        </div>
-
-
-    )
+export default function Saved({ favourites, setFavourites }) {
+  const deleteFromfavourites = (recipe) => {
+    setFavourites(
+      favourites.filter((favourite) => favourite.title !== recipe.title)
+    );
+  };
+  function print() {
+    window.print(favourites);
+  }
+  return (
+    <div className="favouritesContainer">
+      <div className="favourites">
+        {favourites.map((favourite, index) => (
+          <Recipe
+            key={index}
+            title={favourite.title}
+            calories={favourite.calories}
+            image={favourite.image}
+            ingredients={favourite.ingredients}
+            onClick={deleteFromfavourites}
+            buttonText="DELETE"
+          />
+        ))}
+      </div>
+      <span className="print" onClick={print}>
+        PRINT
+      </span>
+    </div>
+  );
 }
