00852e56924a2b1a2a78ef4a03b646d3580ca677
ordegani <oradeganii@gmail.com>
2021-08-26 03:19:32 +0300

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,39 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import '../App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 function Navbar() {
-    return (
-        <div>
-            <ul className="navUl">
-                
-                <li><Link className="link" to="/welcome">HappyHour</Link></li>
-                <li><Link className="link" to="/">Search</Link></li>
-                <li><Link className="link" to="/info">More Info</Link></li>
-                <li><Link className="link" to="/surprise">Surprise Me</Link></li>
-                <li><Link className="link" to="/saved">Saved</Link></li>
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <ul className="navUl">
+        <li>
+          <Link className="link" to="/welcome">
+            HappyHour
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/">
+            Search
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/info">
+            More Info
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/surprise">
+            Surprise Me
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/saved">
+            Saved
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;
