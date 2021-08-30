8f2040c6b4e4a2cc7235740f419780d3c7beb2fd
ordegani <oradeganii@gmail.com>
2021-08-30 13:14:39 +0300

    changes

--- src/components/Saved.js
+++ src/components/Saved.js
@@ -13,10 +13,15 @@ export default function Saved({ favourites, setFavourites }) {
   }
   return (
     <div className="favouritesContainer">
-              <button className={favourites.length==0?"hide":"printBtn"} onClick={print}>
+      <button
+        className={favourites.length == 0 ? "hide" : "printBtn"}
+        onClick={print}
+      >
         PRINT
       </button>
-      <h3 className={favourites.length==0?"unSavedMessage":"hide"} >you haven't saved anything yet</h3>
+      <h3 className={favourites.length == 0 ? "unSavedMessage" : "hide"}>
+        you haven't saved anything yet
+      </h3>
       <div className={"favourites"}>
         {favourites.map((favourite, index) => (
           <Recipe
