e478d1919c811442969dfd93e70589e6a34c24a1
ordegani <oradeganii@gmail.com>
2021-08-31 16:51:49 +0300

    changes

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,7 +8,7 @@ const Info = () => {
         <label>Wize Alcohol Consuming Tips:</label>
         <ul>
           <li>
-            1. Hair of the dog. Drinking to ease the symptoms of a hangover is
+             Hair of the dog. Drinking to ease the symptoms of a hangover is
             sometimes called taking the hair of the dog, or hair of the dog that
             bit you. The notion is that hangovers are a form of alcohol
             withdrawal, so a drink or two will ease the withdrawal. There may be
@@ -22,7 +22,7 @@ const Info = () => {
             a cycle," he says. "It doesn't allow you to recover."
           </li>
           <li>
-            2. Drink fluids. Alcohol promotes urination because it inhibits the
+             Drink fluids. Alcohol promotes urination because it inhibits the
             release of vasopressin, a hormone that decreases the volume of urine
             made by the kidneys. If your hangover includes diarrhea, sweating,
             or vomiting, you may be even more dehydrated. Although nausea can
@@ -30,7 +30,7 @@ const Info = () => {
             water might help your hangover.
           </li>
           <li>
-            3. Get some carbohydrates into your system. Drinking may lower blood
+             Get some carbohydrates into your system. Drinking may lower blood
             sugar levels, so theoretically some of the fatigue and headaches of
             a hangover may be from a brain working without enough of its main
             fuel. Moreover, many people forget to eat when they drink, further
@@ -38,7 +38,7 @@ const Info = () => {
             levels back to normal.
           </li>
           <li>
-            4. Avoid darker-colored alcoholic beverages. Experiments have shown
+             Avoid darker-colored alcoholic beverages. Experiments have shown
             that clear liquors, such as vodka and gin, tend to cause hangovers
             less frequently than dark ones, such as whiskey, red wine, and
             tequila. The main form of alcohol in alcoholic beverages is ethanol,
@@ -49,7 +49,7 @@ const Info = () => {
             hangover.
           </li>
           <li>
-            5. Take a pain reliever, but not Tylenol. Aspirin, ibuprofen
+             Take a pain reliever, but not Tylenol. Aspirin, ibuprofen
             (Motrin, other brands), and other nonsteroidal anti-inflammatory
             drugs (NSAIDs) may help with the headache and the overall achy
             feelings. NSAIDs, though, may irritate a stomach already irritated
@@ -58,13 +58,13 @@ const Info = () => {
             effects on the liver.
           </li>
           <li>
-            6. Drink coffee or tea. Caffeine may not have any special
+             Drink coffee or tea. Caffeine may not have any special
             anti-hangover powers, but as a stimulant, it could help with the
             grogginess. Coffee is a diuretic, though, so it may exacerbate
             dehydration.
           </li>
           <li>
-            7. B vitamins and zinc. A study recently published in The Journal of
+             B vitamins and zinc. A study recently published in The Journal of
             Clinical Medicine evaluated the diets for 24 hours before and after
             excessive drinking occurred. It was a small study and results were
             based on the participants saying what they ate. However, they did
@@ -79,29 +79,29 @@ const Info = () => {
         <label>Wize Diet Tips:</label>
         <ul>
           <li>
-            1. Eat breakfast: A protein and healthy fat breakfast can keep you
+             Eat breakfast: A protein and healthy fat breakfast can keep you
             full for longer and help prevent snacking during the day.
           </li>
 
           <li>
-            2. Eat regular meals: This can help you burn calories more
+             Eat regular meals: This can help you burn calories more
             effectively and helps prevent mindless snacking.
           </li>
 
           <li>
-            3. Remember your “five-a-day:” Fruits and vegetables can be a tasty
+             Remember your “five-a-day:” Fruits and vegetables can be a tasty
             snack and they can bulk out your meals. They are high in nutrients
             and fiber and low in calories and fat.
           </li>
 
           <li>
-            4. Eat slow-burning calories: High-fiber carbohydrates, such as
+             Eat slow-burning calories: High-fiber carbohydrates, such as
             legumes, and healthy fats, such as avocado, take longer to release
             energy, so you will not get hungry as quickly.
           </li>
 
           <li>
-            5. Exercise: This can help burn off extra calories, and it can make
+             Exercise: This can help burn off extra calories, and it can make
             you feel good. A brisk daily walk is easy for most people to do and
             costs nothing. Challenge yourself with a pedometer. For people who
             use a wheelchair, there are exercises that can boost heart health
@@ -109,14 +109,14 @@ const Info = () => {
           </li>
 
           <li>
-            6. Drink water: It is healthful, has no calories, and can fill you
+             Drink water: It is healthful, has no calories, and can fill you
             up. Avoid alcohol and sodas as these can easily provide far too many
             calories. If you crave sweet drinks, choose unsweetened fruit
             juices, or better still, get a juice maker.
           </li>
 
           <li>
-            7. Eat more fiber: Fiber, found in fruits, vegetables, and
+             Eat more fiber: Fiber, found in fruits, vegetables, and
             wholegrains, can help you feel full and encourage healthy digestion.
           </li>
         </ul>
