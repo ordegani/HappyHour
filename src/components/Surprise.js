72f943c8b07b8bd4471640fd39be48610a095401
ordegani <oradeganii@gmail.com>
2021-09-10 00:40:56 +0300

    changes

--- src/components/Surprise.js
+++ src/components/Surprise.js
@@ -15,7 +15,14 @@ export const Surprise = ({ favourites, setFavourites }) => {
     getRecipes();
   }, [query]);
 
-  const flavourArray = ["banana", "apple", "lemon", "campari", "strawberry", "coffee"];
+  const flavourArray = [
+    "banana",
+    "apple",
+    "lemon",
+    "campari",
+    "strawberry",
+    "coffee",
+  ];
   const ran = Math.floor(Math.random() * flavourArray.length);
   const searcher = flavourArray[ran];
 
