872f00f687dd77034e8993259a9221b3870bb40c
ordegani <oradeganii@gmail.com>
2021-09-16 23:34:29 +0300

    changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -20,7 +20,7 @@ const Welcome = () => {
         Save cocktails you want to make by clicking on the 'save' button. <br />
         🍸
         <br />
-        You can mark the recipe's ingredients that are missing in your party's kitchen, and print it for the next shopping day
+        You can mark the recipe's ingredients that are missing from your party's kitchen, and print it for your next shopping day
         🍸
         <br />
         Please Check the Info section for tips on how to drink and diet wizely.
